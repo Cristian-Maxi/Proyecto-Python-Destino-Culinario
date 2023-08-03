23338773deb70004b09f0034ba5c3bdf0770c8b8
Cristian-Maxi <134754887+Cristian-Maxi@users.noreply.github.com>
2023-08-03 02:39:59 -0300

    Add files via upload

---
+++ views/vista_principal.py
@@ -0,0 +1,88 @@
+#vista principal.py
+import tkinter as tk
+from tkinter import messagebox
+from tkintermapview import TkinterMapView
+import customtkinter as ctk
+import json
+from views.vista_DestinoCulinario import VisitasCulinarias
+
+class VistaPrincipal:
+    def __init__(self, root, seleccionar_local_callback=None, seleccionar_DestinoCulinario_callback=None):
+        self.root = root
+        self.seleccionar_local_callback = seleccionar_local_callback
+        self.seleccionar_DestinoCulinario_callback = seleccionar_DestinoCulinario_callback
+        self.locales = []  # Lista de locales
+        self.cargar_locales()
+
+        vcolor="orange" 
+        #comienza dibujo pantalla
+        #self.frame_top = ctk.CTkFrame(self.root, padx=5, pady=1)
+        self.frame_top = ctk.CTkFrame(self.root, width=200, height=200, fg_color=vcolor)
+        self.frame_top.pack(side='top', fill='x')
+        titulo = ctk.CTkLabel(self.frame_top, text="Welcome to Food Travel", font=("Wide Latin", 23), fg_color=vcolor)
+        titulo.pack(pady=40 , padx=20)
+
+        self.frame_locales = ctk.CTkFrame(self.root, width=400, height=300)
+        self.frame_locales.pack(side='left', fill='both', expand=False, padx=10, pady=0)
+
+        self.frame_mapa = ctk.CTkFrame(self.root, width=600, height=600)
+        self.frame_mapa.pack(side='left')
+
+        # Placeholder para el mapa
+        self.mapa = TkinterMapView(self.frame_mapa, width=600, height=600, corner_radius=0)
+        self.mapa.set_position(-24.7858636034, -65.408408504006)  
+        self.mapa.set_zoom(16)
+        self.mapa.pack(side='right')
+
+        # Listbox para los locales
+        self.lista_locales = tk.Listbox(self.frame_locales)
+        self.lista_locales.bind('<<ListboxSelect>>', seleccionar_local_callback)
+        self.lista_locales.pack(fill='both', expand=True)
+        # Crear un frame para centrar el botón y una etiqueta de bienvenida
+        frame_centro = ctk.CTkFrame(self.root)
+        frame_centro.pack(expand=True, fill=ctk.BOTH)
+
+        label_bienvenida= ctk.CTkLabel(frame_centro, text="Bienvenido a nuestra App Culinaria", font=("Arial", 16))
+        label_bienvenida.pack(pady=30)
+
+        boton_abrir_visitas_culinarias = ctk.CTkButton(frame_centro, text="Visitas Culinarias", command=VisitasCulinarias)
+        boton_abrir_visitas_culinarias.pack(pady=50)
+
+        # Botón para mostrar eventos
+        boton_eventos = ctk.CTkButton(self.frame_locales, text="  E v e n t o s  ", command=self.mostrar_eventos)
+        boton_eventos.pack(side='bottom', pady=10)
+
+    def agregar_local(self, local):
+        nombre = local.nombre
+        self.lista_locales.insert(ctk.END, nombre)
+
+    def agregar_marcador_mapa(self, latitud, longitud, texto, imagen=None):
+        return self.mapa.set_marker(latitud, longitud, text=texto, image=imagen, command=self.seleccionar_DestinoCulinario_callback)
+
+    def cargar_locales(self):
+        # Cargar los locales desde el archivo JSON y agregarlos a la lista 'locales'
+        with open("data/locales.json", "r") as archivo:
+            self.locales = json.load(archivo)
+
+    def mostrar_eventos(self):
+        indice_seleccionado = self.lista_locales.curselection()
+        if not indice_seleccionado:
+            messagebox.showinfo("Error", "Seleccione una ubicación antes de ver los eventos.")
+            return
+
+        local_seleccionado = self.locales[indice_seleccionado[0]]
+        eventos = []
+
+        with open("data/actividades.json", "r") as archivo:
+            actividades = json.load(archivo)
+
+        for actividad in actividades:
+            if actividad["id_DestinoCulinario"] == local_seleccionado["id_DestinoCulinario"]:
+                eventos.append(actividad["nombre"])
+
+        if eventos:
+            mensaje = "\n".join(eventos)
+        else:
+            mensaje = "No hay eventos para esta ubicación."
+
+        messagebox.showinfo("Eventos", mensaje)
